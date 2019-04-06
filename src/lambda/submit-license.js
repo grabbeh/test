d26d50fc4f6fa42811b67a9a29cd3ae3831f7ac7
Michael <mbg@outlook.com>
2019-04-06 16:30:27 +0100

    Update

--- src/lambda/submit-license.js
+++ src/lambda/submit-license.js
@@ -11,36 +11,48 @@ export function handler (event, context, callback) {
   }
   var url = JSON.parse(event.body).url
   request({ url, json: true }, (error, response, body) => {
-    var deps = _.keys(body.dependencies)
-    var urls = deps.map(dep => {
-      var version = body.dependencies[dep]
-      if (typeof version === 'string') {
-        if (version.includes('~') || version.includes('^')) {
-          version = version.substr(1)
-        }
-      }
-      return `https://registry.npmjs.org/${dep}/${version}`
-    })
-    var data = []
-    async.each(
-      urls,
-      (url, cb) => {
-        request({ url, json: true }, (error, response, body) => {
-          if (error) console.log(error)
-          data.push(body)
-          cb()
-        })
-      },
-      err => {
-        if (err) {
-          console.log('A file failed to process')
-        } else {
-          callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(data)
-          })
-        }
-      }
-    )
+    if (error) console.log(error)
+    getFullDependencyData(body.dependencies, callback)
+    // extract out so function accepts either package.json file from github or details of data from npm
   })
+}
+
+// dependencies are an object structure of { request: 1.0.0, express: 2.0.0 } etc
+function getFullDependencyData (dependencies, callback) {
+  var deps = _.keys(dependencies)
+  var urls = deps.map(dep => {
+    var version = dependencies[dep]
+    if (typeof version === 'string') {
+      if (version.includes('~') || version.includes('^')) {
+        version = version.substr(1)
+      }
+    }
+    return `https://registry.npmjs.org/${dep}/${version}`
+  })
+  // need to decide data structure for sub-dependencies
+  var data = []
+  async.each(
+    urls,
+    (url, cb) => {
+      request({ url, json: true }, (error, response, dependencyData) => {
+        if (error) console.log(error)
+
+        data.push(dependencyData)
+        if (dependencyData.dependencies) {
+          getFullDependencyData(dependencyData.dependencies)
+        }
+        cb()
+      })
+    },
+    err => {
+      if (err) {
+        console.log('A url failed to process')
+      } else {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(data)
+        })
+      }
+    }
+  )
 }
