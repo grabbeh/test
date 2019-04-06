93189d6c93a563924aabc3d845c6f92e37a34a27
Michael <mbg@outlook.com>
2019-04-06 12:46:48 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,13 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-netlify-functions`,
+      options: {
+        functionsSrc: `${__dirname}/src/lambda`,
+        functionsOutput: `${__dirname}/lambda`
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
