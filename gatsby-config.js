ce7984d322c58f97777a3d52c0be1c2d56f8ce7e
Michael <mbg@outlook.com>
2019-04-08 00:19:17 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-netlify-functions`,
