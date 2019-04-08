ccc8d20b8230c3cd24278f08290a52fa85371d09
Michael <mbg@outlook.com>
2019-04-08 23:30:53 +0100

    Update

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-exports.hander = function (event, context, callback) {
+exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
