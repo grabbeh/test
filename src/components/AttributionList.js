320b1d5585dcef576b589d2e18b7b9d567c151de
Michael <mbg@outlook.com>
2019-05-17 08:31:22 +0100

    Update

--- src/components/AttributionList.js
+++ src/components/AttributionList.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Box from '../components/Box'
+import Text from '../components/Text'
 
 const AttributionList = ({ dependencies }) => {
   return (
     <Box borderBottom='2px solid' borderColor='black'>
       {dependencies.map(d => {
-        return d.licenses.map((l, i) => <Box key={i}>{l.licenseText}</Box>)
+        return d.licenses.map((l, i) => (
+          <Box key={i}>
+            <Text>{d.name}</Text>
+            <Text>{l.licenseText}</Text>
+          </Box>
+        ))
       })}
     </Box>
   )
